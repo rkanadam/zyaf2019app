1306d5e35860ef91ef57cbc95158210f952cc9e7
rkanadam <raghuram.kanadam@gmail.com>
2019-02-23 09:46:05 -0800

    Fixing baseURL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from '@angular/material';
+import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatDialogModule, MatSnackBarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import {MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule} from '@angular/materia
     NgtUniversalModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDialogModule,
   ],
   providers: [
     {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 2500}}
